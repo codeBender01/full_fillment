8d96f4069939d0c7bbd6d0e27680995c98e8c08d
unknown <ahmetsaparow.mr@gmail.com>
2022-08-23 06:03:27 -0700

    Home page components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import "./home.scss";
+
+//images used on Home Page
+
 import order from "../../images/order.png";
 import time from "../../images/time.png";
 import money from "../../images/money.png";
+import chat from "../../images/chat.png";
 import people from "../../images/people.png";
+import nbook from "../../images/notebook.png";
+import phone from "../../images/phone.png";
+import rcycle from "../../images/recycle.png";
+import coin from "../../images/coin.png";
+import plant from "../../images/plant.png";
+import rocket from "../../images/rocket.png";
+import planet from "../../images/planet.png";
+import mountain from "../../images/mountain.png";
+import profile1 from "../../images/profile1.png";
+import profile2 from "../../images/profile2.png";
 
 const stats = [
   {
@@ -49,13 +63,59 @@ const benefits = [
     id: 3,
     title: "Получение большей лояльности клиентов",
     def: "Отличные продукты говорят с вашей аудиторией. Клиенты любят бренд, который приносит пользу и решает их болевые точки.",
-    img: people,
+    img: chat,
   },
   {
     id: 4,
     title: "Повышение узнаваемости бренда",
     def: "У нас есть графические дизайнеры, чтобы улучшить внешний вид упаковки продукта. Потрясающий внешний вид позволяет получить больше прибыли.",
     img: people,
+  },
+];
+
+const chars = [
+  {
+    id: 1,
+    title: "Прозрачный",
+    img: phone,
+  },
+  {
+    id: 2,
+    title: "Эффективный",
+    img: rcycle,
+  },
+  {
+    id: 3,
+    title: "Легко использовать",
+    img: nbook,
+  },
+  {
+    id: 4,
+    title: "Прибыльный",
+    img: coin,
+  },
+];
+
+const plan = [
+  {
+    id: 1,
+    title: "Новичок",
+    img: plant,
+  },
+  {
+    id: 2,
+    title: "Запускать",
+    img: rocket,
+  },
+  {
+    id: 3,
+    title: "Готов к масштабированию",
+    img: planet,
+  },
+  {
+    id: 1,
+    title: "Создайте cвой бренд",
+    img: mountain,
   },
 ];
 
@@ -72,7 +132,7 @@ function Stat(props) {
 function Benefit(props) {
   return (
     <div className="benefit flex">
-      <img src={props.img} alt={props.img.toString()} />
+      <img src={props.img} alt="illustration" />
       <div>
         <h4>{props.title}</h4>
         <p>{props.def}</p>
@@ -123,6 +183,76 @@ class Home extends React.Component {
               />
             );
           })}
+        </div>
+
+        <div className="chars flex">
+          {chars.map((char) => {
+            return (
+              <div className="char flex" key={char.id}>
+                <img src={char.img} alt="illustration" />
+                <span>{char.title}</span>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="business-plan">
+          <div className="title">
+            <h2>Гибкие планы для развития бизнеса</h2>
+          </div>
+
+          <div className="flex illustration">
+            {plan.map((p) => {
+              return (
+                <div key={p.id} className="plan">
+                  <img src={p.img} alt="illustration" />
+                  <h4>{p.title}</h4>
+                  <p>
+                    molestie qui dolor feugait enim suscipit consequat.
+                    hendrerit accumsan et ut duis nibh autem aliquip dolor vel
+                    sed tation delenit wisi et nulla consectetuer te veniam,
+                    quis dignissim zzril ullamcorper at augue lobortis in
+                    praesent dolore nisl luptatum ut ex in facilisi. tincidunt
+                    erat vero Ut{" "}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="customers ">
+          <div className="container flex">
+            <div className="title">
+              <h2> Что говорят наши клиенты</h2>
+              <p>
+                От начинающих предпринимателей до признанных брендов электронной
+                коммерции, узнайте, что говорят наши клиенты о нашей сети
+                фулфилмент-центров 3PL и команде преданных своему делу
+                профессионалов фулфилмента.{" "}
+              </p>
+            </div>
+
+            <div className="customer">
+              <div className="profile-pic">
+                <img src={profile1} alt="" />
+              </div>
+              <div className="feedback">
+                <p>
+                  Уsse veniam, iriure dolor sit vel dolor ut aliquip zzril
+                  consectetuer illum molestie nostrud augue vel feugiat suscipit
+                  ea consequat. aliquam commodo delenit ex diam et magna
+                  ullamcorper laoreet odio illum molestie nostrud augue vel
+                  feugiat suscipit ea consequat. aliquam commodo delenit ex diam
+                  et magna ullamcorper laoreet odio
+                </p>
+
+                <div>
+                  <h5>Имя</h5>
+                  <span>Профессия</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
