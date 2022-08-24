5034a5df1c42f35e6c3bd74e70e2583917797d1d
unknown <ahmetsaparow.mr@gmail.com>
2022-08-24 13:26:36 -0700

    Finishing all 5 pages

--- src/pages/contactUs/contactUs.jsx
+++ src/pages/contactUs/contactUs.jsx
@@ -1,8 +1,57 @@
 import React from "react";
+import Title from "../../components/title/Title";
+import "./contact-us.scss";
 
 class ContactUs extends React.Component {
   render() {
-    return <h1>contactus</h1>;
+    return (
+      <div className="contact-us">
+        <Title text="Свяжитесь с нами" />
+        <div className="line"></div>
+
+        <div className="contacts">
+          <div className="main-requests">
+            <h3>Обшие запросы</h3>
+            <span>Yurdy,Saher ady, koce ady, jay,otag</span>
+            <span>+993(60)-00-00-00</span>
+            <span>ady.karhana@gmail.com</span>
+          </div>
+
+          <div className="hor-line"></div>
+
+          <div className="hotline">
+            <h3>Свяжитесь с нами на прямую</h3>
+            <div>
+              <h5>Имя и Фамилия</h5>
+              <span>+993(60)-00-00-00</span>
+              <span>ady.karhana@gmail.com</span>
+            </div>
+            <div>
+              <h5>Имя и Фамилия</h5>
+              <span>+993(60)-00-00-00</span>
+              <span>ady.karhana@gmail.com</span>
+            </div>
+          </div>
+        </div>
+
+        <form action="URL">
+          <div>
+            <label htmlFor="fullName">Полное имя *</label>
+            <input type="text" id="fullName" />
+          </div>
+          <div>
+            <label htmlFor="e-mail">Электронная почта *</label>
+            <input type="email" id="e-mail" />
+          </div>
+          <div>
+            <label htmlFor="message">Сообщение *</label>
+            <input type="text" id="message" className="textarea" />
+          </div>
+
+          <input type="submit" value="ОТПРАВИТЬ СООБШЕНИЕ" className="submit" />
+        </form>
+      </div>
+    );
   }
 }
 
